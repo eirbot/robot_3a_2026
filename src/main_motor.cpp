b88b0ca6b966cda6cdc783990083ef15f7bbf52b
Quentin Fallito <quentinfallito@gmail.com>
2026-03-09 16:46:31 +0100

    test qui confirme que c'etait le cablage le probleme, pour flasher l'esp il faut debrancher le 3.3v vers COM

--- src/main_motor.cpp
+++ src/main_motor.cpp
@@ -1,1 +1,26 @@
-#include "ClassMotor.hpp"
+// #include <TMC2100.h>
+#include <BasicStepperDriver.h>
+
+#define STEPS 1600
+#define STEP_PIN 4
+#define DIR_PIN 2
+
+
+
+BasicStepperDriver stepper(STEPS, DIR_PIN, STEP_PIN);
+
+void setup() {
+  stepper.setRPM(120);
+  stepper.begin();
+}
+
+void loop() {
+  // Rotate one revolution clockwise
+  stepper.move(360);
+  delay(2000);
+
+  // Rotate one revolution counter-clockwise
+  stepper.move(-360);
+  delay(2000);
+}
+
