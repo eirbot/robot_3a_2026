3a1b4ac147fcea2a98934b049d34900a6c044758
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2026-05-14 21:11:23 +0200

    debug: show decoded callstack with PlatformIO

--- scripts/send_cmd.py
+++ scripts/send_cmd.py
@@ -1,8 +1,12 @@
 import serial
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
+
+# envie de me flusher le buffer
+# ~ Eric Lecktackone (100% j'ai mal écrit ça)
 while ser.in_waiting:
     print(ser.readLine())
+
 ser.write(bytearray(input("Send message : "), encoding='ascii'))
 
 while True:
